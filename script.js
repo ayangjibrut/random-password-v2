cb25929824c26da2d14e996ac94952acd03636e8
Rizza Marwan <rizzamarwan@icloud.com>
2024-09-19 21:44:02 +0700

    Update
    disable user scalable

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","^","*","_","-","+","=","|",":","<",">","."]
 
-let passwordLength = 13
+const alphabetsSymbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","^","*","_","-","+","=","|",":","<",">","."]
+
+const alphabetsNumbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+// "/"]
+
+let passwordLength = 15
 
 let generateEl = document.querySelector("#generate-el")
 let passwordOne = document.querySelector("#password-one")
@@ -9,27 +15,18 @@ let passwordTwo = document.querySelector("#password-two")
 
 let copyText = document.querySelector(".password").innerHTML
 
-generateEl.addEventListener("click", function() {
-    function getRandomCharacter() {
-        let generateCharacter = Math.floor(Math.random() * characters.length)
-        return characters[generateCharacter]
+function getRandomCharacter() {
+    let generateCharacter = Math.floor(Math.random() * characters.length)
+    return characters[generateCharacter]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()
     }
-
-    function generateRandomPassword() {
-        let randomPassword = ""
-        for (let i = 0; i < passwordLength; i++) {
-            randomPassword += getRandomCharacter()
-        }
-        return randomPassword
-    }
-
-    const generatePasswordOne = generateRandomPassword()
-    const generatePasswordTwo = generateRandomPassword()
-
-    passwordOne.textContent = generatePasswordOne
-    passwordTwo.textContent = generatePasswordTwo
-    
-})
+    return randomPassword
+}
 
 const copyContent = async () => {
     try {
@@ -39,3 +36,11 @@ const copyContent = async () => {
         console.error('Failed to copy: ', err)
     }
 }
+
+generateEl.addEventListener("click", function() {
+    const generatePasswordOne = generateRandomPassword()
+    const generatePasswordTwo = generateRandomPassword()
+
+    passwordOne.textContent = generatePasswordOne
+    passwordTwo.textContent = generatePasswordTwo
+})
